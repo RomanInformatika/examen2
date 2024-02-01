efce48f341657dc7d47d3abb844c8a085de7d57f
Лаврентьев Роман Владимирович <lavrentevrv@szao.obr.mos.ru>
2024-02-01 13:10:01 +0300

    Zadanie 2

--- main.py
+++ main.py
@@ -24,3 +24,24 @@ f=open("student_new.csv","w")
 for el in a:
     f.write(','.join(el)+'\n')
 #########################################################################
+
+for i in range(1,len(a)):
+    x=a[i]
+    j=i
+    while j>0 and float(a[j - 1][4]) > float(x[4]):
+        a[j] = a[j - 1]
+        j-=1
+    a[j]=x
+
+
+b=[]
+for el in a:
+    if '10' in el[3] and el[4]=='5':
+        b.append(el)
+
+
+
+for i in range(3):
+
+    shortName=getShortName(b[i][1])
+    print(str(i+1)+' место: '+shortName)
